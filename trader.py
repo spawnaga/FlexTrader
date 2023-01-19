26f4a4ed488c665c8b8594f4850a01a58e06e009
spawnaga <spawnaga@gmail.com>
2023-01-18 19:03:08 -0700

    first commit

---
+++ trader.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 20:46:39 2023
+
+@author: alial
+"""
+
+# from indicators import indicators_dataframe
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+from ib_insync import *
+util.startLoop()
+
+class Trader:
+    """Class for interacting with Interactive Brokers Gateway"""
+    def __init__(self, history_length=10):
+        # Connect to IB gateway
+        self.ib = IB()
+        self.ib.connect('127.0.0.1', 7496, clientId=np.random.randint(0,1000))
+        
+        # Initialize attributes
+        self.profit = 0
+        self.num_trades = 0
+        self.num_wins = 0
+        self.num_losses = 0
+        self.max_profit = 0
+        self.max_loss = 0
+        self.capital = 1000000
+        self.cash = 1000000
+        self.num_contracts = 0
+        self.total_value =self.cash+(self.num_contracts*20)
+        self.close_prices=[]
+        self.trades=[]
+        self.priceAtStart=0
+        self.priceAtClose=0
+        self.realized_profit_loss=0
+        self.unreliazed_profit_loss=0
+        
+    def _get_kelly_criterion(self, win_loss_ratio, avg_profit):
+        """Calculate the optimal number of contracts to trade using Kelly Criterion"""
+        return win_loss_ratio - (1 - win_loss_ratio) / (avg_profit / 20)
+
+    def trade(self, contract, action, market, i,row, previous_row):
+        """Execute a trade based on the current market state and the output of the model"""
+        realizedPNL = 0
+        # Get close price from Market object
+        close_price = row['close']
+        previous_close_price = previous_row['close']
+        
+        # Calculate number of contracts to trade using fixed-fractional method
+        if self.num_contracts ==0 and action ==0:
+            self.num_contracts = int(self.cash / close_price/20)
+            self.cash = self.cash - abs(self.num_contracts*close_price*20)
+            self.priceAtStart=close_price
+            self.num_trades += 1
+            Action = "buy"
+        elif self.num_contracts ==0 and action ==1:
+            self.num_contracts = -1*int(self.cash / close_price/20)
+            self.cash = abs(-1*self.cash - self.num_contracts*close_price*20)
+            self.priceAtStart=close_price
+            self.num_trades += 1
+            Action = "sell"
+        elif self.num_contracts >0 and action ==3:
+            self.priceAtClose=close_price
+            Action = "sell to close Longs"
+            # print(f"*******Trade {self.num_trades} got {round((self.priceAtClose-self.priceAtStart)*20* self.num_contracts,2)} return*******")
+            # print(f"********************************** Account value is {self.total_value +  round((self.priceAtClose-self.priceAtStart)*20* self.num_contracts,2)} **********************************")
+            realizedPNL = (self.priceAtClose-self.priceAtStart)*20* self.num_contracts
+            self.num_contracts = 0
+            self.cash = abs(self.total_value) + realizedPNL
+            if realizedPNL > 0:
+                self.num_wins += 1
+            else:
+                self.num_losses += 1
+            self.priceAtStart =0
+        elif self.num_contracts <0 and action ==4:
+            self.priceAtClose=close_price
+            Action = "buy to close Shorts"
+            # print(f"*******Trade {self.num_trades} got {round((self.priceAtStart-self.priceAtClose)*20*self.num_contracts,2)} return*******")
+            # print(f"********************************** Account value is {self.total_value +  round((self.priceAtStart-self.priceAtClose)*20*self.num_contracts,2)} **********************************")
+            realizedPNL = (self.priceAtStart-self.priceAtClose)*20*self.num_contracts
+            self.num_contracts = 0
+            self.cash = abs(self.total_value) + realizedPNL
+            if realizedPNL > 0:
+                self.num_wins += 1
+            else:
+                self.num_losses += 1
+            self.priceAtStart =0
+        else:       
+            action = 2
+            Action = "hold"
+        
+        # Calculate profit or loss of trade
+        unreliazed_profit_loss = self.num_contracts * (close_price - self.priceAtStart) *20
+        
+        if realizedPNL !=0:
+            self.realized_profit_loss +=realizedPNL
+
+        # Update performance metrics
+        self.profit = unreliazed_profit_loss+self.realized_profit_loss
+        self.unreliazed_profit_loss = unreliazed_profit_loss
+        
+        self.max_profit = max(self.max_profit, self.realized_profit_loss)
+        self.max_loss = min(self.max_loss, self.realized_profit_loss)
+        self.total_value = self.capital + self.profit
+
+        # print(f'Trade: {self.num_trades}, last close price: {close_price}, previous close price: {previous_close_price}, Action: {Action}, prices differences: { close_price - previous_close_price}; Unrealized Profit/Loss: {round(self.unreliazed_profit_loss,2)}, Realized PNL: {round(self.realized_profit_loss,2)}, Total PNL: {round(self.realized_profit_loss+self.unreliazed_profit_loss,2)}, Account NQ contracts holding = {self.num_contracts}, Account cash balance = {round(self.cash,2)}, Account total value = {round(self.total_value,2)} \n')
+
+        return self.profit
+    
+    def _get_kelly_criterion(self, win_loss_ratio, avg_profit):
+        """Calculate number of contracts to trade using Kelly Criterion"""
+        f = win_loss_ratio - 1
+        if avg_profit==0:
+            avg_profit = 1
+        b = avg_profit / 20
+        kelly_criterion = f / b
+        return kelly_criterion
+
+
+    def disconnect(self):
+        """Method for disconnect"""
+        self.ib.disconnect()
+        
+
+
+class Market:
+    """Class for handling market data"""
+    def __init__(self, trader, contract, history_length=1):
+        # Store the history length as an instance variable
+        self.history_length = history_length
+        self.trader = trader
+        # Get the contract and data
+        self.contract = contract
+        self.scaler = MinMaxScaler(feature_range=(0, 4))
+        trader.ib.qualifyContracts(self.contract)
+        
+    def update_data(self):
+        # Download historical data using reqHistoricalData
+        self.bars = self.trader.ib.reqHistoricalData(
+            self.contract, endDateTime='', durationStr=f'{self.history_length} M',
+            barSizeSetting='5 mins', whatToShow='TRADES',
+            useRTH=False
+        )
+        
+        # Create a DataFrame from the downloaded data
+        df = util.df(self.bars)
+        # df = self.get_analysis(df)
+        df.reset_index(inplace = True,drop=True)
+        # df = df.drop(df.iloc[:,10:],axis=1)
+        df['contract']=0
+        df=df.drop(['date','average'],axis=1)
+        self.df = df
+        self.data = self.scaler.fit_transform(df)
+        
+    def get_state(self, i=0):
+        """Method for getting current state of market"""
+        state = self.data[i]
+        contracts_holding = self.trader.num_contracts
+        state[-1]=contracts_holding
+        np.append(state, i + 2 >= len(self.data))
+        return np.expand_dims(state, 0)
+    
+    # def get_analysis(self, df):
+    #     # Analyze using Technical Analysis on talib libraries
+    #     df = indicators_dataframe(df)
+    #     df = df[df.columns[4:]]
+    #     df = df.drop('volume',axis=1)
+    #     df = df.dropna()
+    #     return df
+    
+    def get_df(self):
+        """Method for returning the DataFrame of market data"""
+        return self.df
+
---
+++ trainer.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 01:45:26 2023
+
+@author: alial
+"""
+
+from collections import deque
+import matplotlib.pyplot as plt
+from trader import Trader, Market
+from ib_insync import ContFuture
+from agents import MultiTask
+import numpy as np
+from multiprocessing import Pool
+
+def train(task):
+    # Initialize the Trader object and connect to the IB gateway
+    # print('*********')
+    trader = Trader()
+    contract = ContFuture('NQ', 'CME')
+    trader.ib.qualifyContracts(contract)
+    
+    # Initialize the DQN agent
+    action_size = 5
+    batch_size = 100
+    previous_row = None
+
+    rewards = [0]
+    steps = [0]
+    # Initialize the rolling window to store the last n rewards
+    window_size = 100
+    rolling_window = deque(maxlen=window_size)
+    # Initialize the list to store the rolling average of the rewards
+    rolling_average = []
+    j=0
+    # Update market data and get the DataFrame
+    pnl=[0]
+    while not trader.profit >= 1000000*0.3 or not trader.num_trades >= 1000:
+        j+=1
+        # while not trader.profit >= 1000000*0.3 or not trader.num_trades >= 1000:
+        market = Market(trader, contract, history_length=1)
+        market.update_data()
+        df = market.get_df()
+        state = market.get_state(0).reshape(-1,7)
+        state_size = state.shape[1]
+        agent = MultiTask(state=state, num_outputs_1=action_size, num_outputs_2 = action_size, state_size=state_size, action_size=action_size, task=task)
+    
+        
+        # if agent.dqn_memory ==deque([]):
+        #     agent.load("trial1",task)
+        for i, row in df.iterrows():
+            if previous_row is None:
+                previous_row = row
+            done = i + 2 >= len(df)
+            if done:
+                break
+            # if i>35: break
+    
+            # Get the current and next states
+            next_state = market.get_state(i+1).reshape(-1,7)
+            # Predict the action using the model
+            action = agent.act(state=state.reshape(-1,7), task= task)
+            # Execute the trade and get the reward
+            reward = trader.trade(contract, action, market, i,row, previous_row)
+            # Append the total reward and number of steps for this episode to the lists
+            rewards.append(reward)
+            steps.append(i)
+            agent.add_to_memory(task,state, action, reward, next_state, done)
+            # if i>35: break
+    
+            rolling_window.append(trader.realized_profit_loss)
+    
+            # Calculate the rolling average of the rewards and append it to the list
+            rolling_average.append(np.mean(rolling_window))
+    
+            # Set the current state to the next state
+            state = next_state
+    
+            if trader.total_value > max(pnl):
+                pnl.append(trader.total_value)
+                # Calculate the mean and standard deviation of the rewards and steps
+                mean_reward = np.mean(rewards)
+                std_reward = np.std(rewards)
+                mean_steps = np.mean(steps)
+                std_steps = np.std(steps)
+    
+                # Print the results
+                # print(f"Mean reward per episode: {mean_reward:.2f} +/- {std_reward:.2f}")
+                # print(f"Mean steps per episode: {mean_steps:.2f} +/- {std_steps:.2f}")
+                plt.plot(rolling_average)
+                # plt.show()
+                agent.save('trial1', task)
+                replay_functions = {
+                                    "dqn": agent.replay_dqn,
+                                    "ddqn": agent.replay_ddqn,
+                                    "actor_critic": agent.replay_actor_critic,
+                                    "policy_gradient": agent.replay_policy_gradient
+                                }
+                replay_functions[task](batch_size)
+                print(f"***************** Episode {j} of {task} final account was {trader.total_value} which is a total profit/loss of {trader.total_value - trader.capital}")
+    return pnl
+
+
+if __name__ == '__main__':
+    # for i in range(10):
+    with Pool(4) as p:
+        results = [p.map(train, ['dqn', 'ddqn', 'actor_critic','policy_gradient'])]
+        print(results)
+
+
