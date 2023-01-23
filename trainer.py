b0a64243ae330bdfebbf0b9237404c649e955203
spawnaga <spawnaga@gmail.com>
2023-01-23 16:20:37 -0700

    adjusted memory saving to add each time new trainings performed on data

--- trainer.py
+++ trainer.py
@@ -12,15 +12,18 @@ from ib_insync import ContFuture
 from agents import MultiTask
 import numpy as np
 from multiprocessing import Pool
+import gc
+
 
 def train(task):
     # Initialize the Trader object and connect to the IB gateway
     # print('*********')
+    levels = {0: 0.005, 1: 0.01, 2: 0.02, 3: 0.03, 4: 0.04, 5: 0.05, 6: 0.1, 7: 0.15, 8: 0.2, 9: 0.25}
     trader = Trader()
-    
+
     # Initialize the DQN agent
     action_size = 5
-    batch_size = 100
+
     previous_row = None
 
     rewards = [0]
@@ -28,69 +31,93 @@ def train(task):
     # Initialize the rolling window to store the last n rewards
     window_size = 100
     rolling_window = deque(maxlen=window_size)
-    # Initialize the list to store the rolling average of the rewards
+    # Initialize the list to store the rolling average of the rewards, profits, iterations and batch size levels
     rolling_average = []
-    j=0
-    # Update market data and get the DataFrame
-    pnl=[0]
-    while not trader.profit >= 1000000*0.3 or not trader.num_trades >= 1000:
-        j+=1
-        # while not trader.profit >= 1000000*0.3 or not trader.num_trades >= 1000:
+    pnl = [0]
+    current_batch_size_level = 0
+    current_iteration = 0
+    batch_size = 0
+
+    while not trader.profit >= 1000000 * 0.3 or not trader.num_trades >= 1000:
+        # start first iteration
+        current_iteration += 1
+        # initialize market and get the dataframe
         market = Market(trader, history_length=1)
         market.update_data()
         df = market.get_df()
+
+        # Get the current and next states
         state = market.get_state(0)
         state_size = state.shape[1]
-        agent = MultiTask(task=task, state=state, num_outputs_1=action_size, num_outputs_2 = action_size, state_size=state_size, action_size=action_size)
+
+
+        agent = MultiTask(task=task, state=state, num_outputs_1=action_size, num_outputs_2=action_size,
+                          state_size=state_size, action_size=action_size)
         # Load saved trainings and memories from previous sessions
-        if not eval(f'agent.{task}_memory'):
+        if eval(f'agent.{task}_memory._size()') == 0:
             agent.load(name='trial1', task=task)
+        for level, percentage in levels.items():
+            if int(len(df) * percentage) < eval(f'agent.{task}_memory._size()'):
+                batch_size = int(len(df) * percentage)
+
         for i, row in df.iterrows():
             if previous_row is None:
                 previous_row = row
             done = i + 2 >= len(df)
             if done:
                 break
-            # if i>35: break
-            # Get the current and next states
-            next_state = market.get_state(i+1)
+            if i == int(len(df) * levels[current_batch_size_level]) and batch_size <= len(df) * levels[
+                current_batch_size_level]:
+                batch_size = int(len(df) * levels[current_batch_size_level])
+                print(
+                    f'Level {list(levels.keys())[current_batch_size_level] - 1} is done. Batch size now is {batch_size} ({levels[current_batch_size_level] * 100}% of the data)')
+                current_batch_size_level += 1
+
+            # Get nextstate value
+            next_state = market.get_state(i + 1)
+
             # Predict the action using the model
-            action = agent.act(state=state, task= task, job='train')
+            action = agent.act(state=state, task=task, job='train')
             print(f'iterate {i} of {task} yielded {action}')
             # Execute the trade and get the reward
-            reward = trader.trade(action, market, i,row, previous_row)
+            reward = trader.trade(action, market, i, row, previous_row)
             # Append the total reward and number of steps for this episode to the lists
             rewards.append(reward)
             steps.append(i)
-            agent.add_to_memory(task,state, action, reward, next_state, done)
+            agent.add_to_memory(task, state, action, reward, next_state, done)
             # if i>35: break
-    
+
             rolling_window.append(trader.realized_profit_loss)
-    
+
             # Calculate the rolling average of the rewards and append it to the list
             rolling_average.append(np.mean(rolling_window))
-    
+
             # Set the current state to the next state
             state = next_state
-    
+
             plt.plot(rolling_average)
 
+            # Update learning of models
             replay_functions = {
-                                "dqn": agent.replay_dqn,
-                                "ddqn": agent.replay_ddqn,
-                                "actor_critic": agent.replay_actor_critic,
-                                "policy_gradient": agent.replay_policy_gradient
-                            }
+                "dqn": agent.replay_dqn,
+                "ddqn": agent.replay_ddqn,
+                "actor_critic": agent.replay_actor_critic,
+                "policy_gradient": agent.replay_policy_gradient
+            }
             replay_functions[task](batch_size)
+            # Save the update agents
             agent.save('trial1', task)
-            print(f"***************** Episode {j} of {task} final account was {trader.total_value} which is a total profit/loss of {trader.total_value - trader.capital}")
+            # Garbage data disposal
+            gc.collect()
+            # Print progress
+            print(
+                f"***************** Episode {current_iteration} of {task} final account was {trader.total_value} which is a total profit/loss of {trader.total_value - trader.capital}")
     return pnl
 
 
 if __name__ == '__main__':
-    # for i in range(10):
+    # results=train(task="policy_gradient")
+    # # for i in range(10):
     with Pool(4) as p:
-        results = [p.map(train, ["dqn","ddqn","actor_critic","policy_gradient"])]
+        results = [p.map(train, ['dqn', 'ddqn', 'actor_critic', 'policy_gradient'])]
         print(results)
-
-
